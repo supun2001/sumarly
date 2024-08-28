761a1cbff6c4ed266cf134e572d52d4e318ced9a
Supun hasanka <38599870+supun2001@users.noreply.github.com>
2024-08-29 00:31:54 +0530

    added routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,44 @@
-import React from "react"
+import {React, useState} from "react"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login."
+import Register from "./pages/Register"
+import Notfount from "./pages/Notfount"
+import ProtectedRoute from './components/ProtectedRoute'
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to="/login" />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/"
+            element={
+                <Home />
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/logout" element={<Logout />} />
+          <Route path="/register" element={<RegisterAndLogout />} />
+          <Route path="*" element={
+            <ProtectedRoute>
+              <Notfount />
+              </ProtectedRoute>
+            }></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ frontend/src/pages/Notfount.jsx
@@ -0,0 +1,5 @@
+function Notfount(){
+    return <div>Notfount</div>
+}
+
+export default Notfount
